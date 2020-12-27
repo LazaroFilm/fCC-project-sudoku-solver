a063dd03bcc1c14e9aae4d1f9cdfde362eaca3bf
Victor Lazaro <lazarofilm@gmail.com>
2020-12-27 12:21:33 -0500

    make it better

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -3,9 +3,11 @@ class SudokuSolver {
     console.log("validating:");
     try {
       if (!puzzleString.match(/^[\d.]{81}$/)) throw "invalid Sudoku string";
+
       return true;
     } catch (err) {
       console.log(err);
+      return false;
     }
   }
 
