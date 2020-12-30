556abc8b9a04320b6cc482d287ed3e2f2e6e6130
Victor Lazaro <lazarofilm@gmail.com>
2020-12-30 12:04:03 -0500

    added func test for check ALL DONE!

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -97,4 +97,192 @@ suite("Functional Tests", () => {
         });
     });
   });
+  suite("POST request to /api/check", () => {
+    const url = "/api/check";
+    test("Check a puzzle placement with all fields", () => {
+      chai
+        .request(server)
+        .post(url)
+        .set("content-type", "application/x-www-form-urlencoded")
+        .send({
+          puzzle: goodPuzzle,
+          coordinate: "B1",
+          value: "3",
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200, "status should be 200");
+          assert.isObject(res.body, "should be an object");
+          assert.hasAllKeys(res.body, "valid", "should only return valid");
+          assert.isTrue(res.body.valid, "valid should return true");
+        });
+    });
+    test("Check a puzzle placement with single placement conflict", () => {
+      chai
+        .request(server)
+        .post(url)
+        .set("content-type", "application/x-www-form-urlencoded")
+        .send({
+          puzzle: goodPuzzle,
+          coordinate: "B1",
+          value: "8",
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200, "status should be 200");
+          assert.isObject(res.body, "should be an object");
+          assert.hasAllKeys(
+            res.body,
+            ["valid", "conflict"],
+            "should return valid & conflict"
+          );
+          assert.isFalse(res.body.valid, "valid should return false");
+          assert.deepEqual(
+            res.body.conflict,
+            ["row"],
+            'should only return "row" as conflict'
+          );
+        });
+    });
+    test("Check a puzzle placement with multiple placement conflicts", () => {
+      chai
+        .request(server)
+        .post(url)
+        .set("content-type", "application/x-www-form-urlencoded")
+        .send({
+          puzzle: goodPuzzle,
+          coordinate: "B1",
+          value: "6",
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200, "status should be 200");
+          assert.isObject(res.body, "should be an object");
+          assert.hasAllKeys(
+            res.body,
+            ["valid", "conflict"],
+            "should return valid & conflict"
+          );
+          assert.isFalse(res.body.valid, "valid should return false");
+          assert.deepEqual(
+            res.body.conflict,
+            ["column", "region"],
+            'should return both "column" & "region" as conflicts'
+          );
+        });
+    });
+    test("Check a puzzle placement with multiple placement conflicts", () => {
+      chai
+        .request(server)
+        .post(url)
+        .set("content-type", "application/x-www-form-urlencoded")
+        .send({
+          puzzle: goodPuzzle,
+          coordinate: "B5",
+          value: "7",
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200, "status should be 200");
+          assert.isObject(res.body, "should be an object");
+          assert.hasAllKeys(
+            res.body,
+            ["valid", "conflict"],
+            "should return valid & conflict"
+          );
+          assert.isFalse(res.body.valid, "valid should return false");
+          assert.deepEqual(
+            res.body.conflict,
+            ["row", "column", "region"],
+            "should return all three as conflicts"
+          );
+        });
+    });
+    test("Check a puzzle placement with missing required fields", () => {
+      chai
+        .request(server)
+        .post(url)
+        .set("content-type", "application/x-www-form-urlencoded")
+        .send({
+          puzzle: goodPuzzle,
+          value: "7",
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200, "status should be 200");
+          assert.isObject(res.body, "should be an object");
+          assert.hasAllKeys(res.body, "error", "should only return error");
+          assert.propertyVal(res.body, "error", "Required field(s) missing");
+        });
+    });
+    test("Check a puzzle placement with invalid characters", () => {
+      const badCharacters = goodPuzzle.replace(/^\S{6}/, "sudoku");
+      chai
+        .request(server)
+        .post(url)
+        .set("content-type", "application/x-www-form-urlencoded")
+        .send({
+          puzzle: badCharacters,
+          coordinate: "B1",
+          value: "3",
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200, "status should be 200");
+          assert.isObject(res.body, "should be an object");
+          assert.hasAllKeys(res.body, "error", "should only return error");
+          assert.propertyVal(res.body, "error", "Invalid characters in puzzle");
+        });
+    });
+    test("Check a puzzle placement with incorrect length", () => {
+      const tooLong = `1.2.3.4.${goodPuzzle}`;
+      chai
+        .request(server)
+        .post(url)
+        .set("content-type", "application/x-www-form-urlencoded")
+        .send({
+          puzzle: tooLong,
+          coordinate: "B1",
+          value: "3",
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200, "status should be 200");
+          assert.isObject(res.body, "should be an object");
+          assert.hasAllKeys(res.body, "error", "should only return error");
+          assert.propertyVal(
+            res.body,
+            "error",
+            "Expected puzzle to be 81 characters long"
+          );
+        });
+    });
+    test("Check a puzzle placement with invalid placement coordinate:", () => {
+      chai
+        .request(server)
+        .post(url)
+        .set("content-type", "application/x-www-form-urlencoded")
+        .send({
+          puzzle: goodPuzzle,
+          coordinate: "BOB69",
+          value: "7",
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200, "status should be 200");
+          assert.isObject(res.body, "should be an object");
+          assert.hasAllKeys(res.body, "error", "should only return error");
+          assert.propertyVal(res.body, "error", "Invalid coordinate");
+        });
+    });
+    test("Check a puzzle placement with invalid placement value:", () => {
+      chai
+        .request(server)
+        .post(url)
+        .set("content-type", "application/x-www-form-urlencoded")
+        .send({
+          puzzle: goodPuzzle,
+          coordinate: "B1",
+          value: "Hello",
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200, "status should be 200");
+          assert.isObject(res.body, "should be an object");
+          assert.hasAllKeys(res.body, "error", "should only return error");
+          assert.propertyVal(res.body, "error", "Invalid value");
+        });
+    });
+  });
 });
